166a489c0b6d3416af71b7a7dc1cf133046e5574
root <root@d0ad6b3927e9>
2020-11-05 22:59:38 +0000

    loops

--- src/_02_nested_loops/_3_for_loop_gauntlet/Loops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Loops.java
@@ -55,8 +55,72 @@ int birthyear =2007;
 	   System.out.println("in " + start +" I was "+ (start-birthyear )+" years old");
 	   
    }
-    
-    
+	System.out.println("**********************************************");
+
+    for(int f=0;f<3;f++) {
+        for(int g=0;g<3;g++) {
+        	System.out.println(f + ", " + g);
+	    }
+    }
+	System.out.println("**********************************************");
+    for(int h=1;h<10;h++) {
+    	System.out.print(h+ " ");
+        if(h%3==0) {
+        	System.out.println();
+        }
+    }
+	System.out.println("**********************************************");
+	for(int row=1;row<10;row+=3) {
+		for(int col=row;col<row+3;col++) {
+			System.out.print(col +" ");
+		}
+		System.out.println();
+	}
+	System.out.println("**********************************************");
+	//row    //col
+	//1 =-> 1 < 11
+	//11     11 < 21    
+	//21     21 < 31    21 22 23 24... 30
+	for(int row=1;row<101;row+=10) {
+		for(int col=row;col<row+10; col++) {
+			System.out.print(col + " ");
+		}
+		System.out.println();
+	}
+	System.out.println("**********************************************");
+	for(int i =1; i<7; i++) {
+		for(int o=0; o<i;o++) {
+			System.out.print("*");
+			
+		}
+		System.out.println();
+	}
+	System.out.println("**********************************************");
+	for(int i=0;i>-101;i--) {
+	    System.out.println(i + 100);
+	}
 }
 }
+//100 = 100 - 0 
+//99  = 100 - 1
+//98  = 100 - 2
+//97  = 3
+
+
+
+
+
+
+
+/**
+ * for(int i = 0; i < 3; i++) {
+ * 		for(int j = 0; j < 3; j++) { 
+ * 		
+ * 		}
+ * }
+ * i = 0, j = 0,
+ * i = 0, j = 1,
+ * i = 0, j = 2,
+ * i = 1, j = 0
+ */
 
